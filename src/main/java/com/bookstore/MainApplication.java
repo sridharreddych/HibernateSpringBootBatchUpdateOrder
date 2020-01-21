58045bc4f15db6291b67d4d0d3857aa12b9b77ef
SridharChallabotla <sridhar.challabotla@metlife.com>
2020-01-21 18:21:09 +0530

    first commit

---
+++ src/main/java/com/bookstore/MainApplication.java
@@ -0,0 +1,50 @@
+package com.bookstore;
+
+import com.bookstore.service.BookstoreService;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MainApplication {
+
+    private final BookstoreService bookstoreService;
+
+    public MainApplication(BookstoreService bookstoreService) {
+        this.bookstoreService = bookstoreService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+
+            bookstoreService.persistAuthorsAndBooks();
+            bookstoreService.updateAuthorsAndBooks();
+            // bookstoreService.updateBooksAndAuthors();
+        };
+    }
+}
+
+
+/*
+
+How To Optimize Batch Updates Of Parent-Child Relationships In MySQL
+
+Description: Batch updates of entities with associations in MySQL.
+
+Key points:
+
+in application.properties set spring.jpa.properties.hibernate.jdbc.batch_size
+in application.properties set JDBC URL with rewriteBatchedStatements=true (optimization for MySQL, statements get rewritten into a single string buffer and sent in a single request)
+in application.properties set JDBC URL with cachePrepStmts=true (enable caching and is useful if you decide to set prepStmtCacheSize, prepStmtCacheSqlLimit, etc as well; without this setting the cache is disabled)
+in application.properties set JDBC URL with useServerPrepStmts=true (this way you switch to server-side prepared statements (may lead to signnificant performance boost))
+for parent-child relationship with cascade all/persist (e.g. one-to-many, many-to-many) then consider to set up spring.jpa.properties.hibernate.order_updates=true to optimize the batching by ordering updates
+before Hibernate 5, we need to set in application.properties a setting for enabling batching for versioned entities during update and delete operations (entities that contains @Version for implicit optimistic locking); this setting is: spring.jpa.properties.hibernate.jdbc.batch_versioned_data=true; starting with Hibernate 5, this setting should be true by default
+Output example for parent-child relationship:
+
+*/
